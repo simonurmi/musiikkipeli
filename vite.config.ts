a0ad71913dd85d39ac58b6137055ce9d68cad8e3
Simo Nurmi <simo.aleksi.nurmi@gmail.com>
2025-10-31 18:10:38 +0200

    Add @netlify/vite-plugin-tanstack-start

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   plugins: [
@@ -12,6 +13,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
   ],
 });
